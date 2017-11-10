c363e33460a4da03a0f9b1e83f4d4c80bc4ae17a
Guus Claessen <combatgup@hotmail.com>
2017-11-10 11:07:22 +0100

    init basic server

---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+const EventHubClient = require('azure-event-hubs').Client;
+const express = require('express')
+const bodyParser = require('body-parser');  
+const url = require('url');  
+const querystring = require('querystring');  
+const connectToAzure = false;
+
+const getDevValues = (n) => {
+    const now = Date.now();
+    const values = [];
+    values.push({time: now, value: Math.random()});
+
+    for (let i = 1; i < n; i++) { 
+        const item = {time: now - (i * 60000), value: Math.random()};
+        values.unshift(item);
+    }
+
+    return values;
+
+} 
+
+const connectionString = '';
+
+const printError = (err) => {
+    console.log(err.message);
+};
+
+const printMessage = (message) => {
+    console.log('Message received: ');
+    const messageJson = JSON.stringify(message.body);
+    const messageObject = JSON.parse(messageJson);
+    let messageToPrint = messageObject;
+
+    if(messageObject.type === "Buffer" && messageObject.data) {
+        const messageBuffer = Buffer.from(messageObject.data);
+        messageToPrint = messageBuffer.toString();
+    }
+    console.log(messageToPrint);
+    console.log('');
+};
+
+const getClientReceiver = (partitionId) => {
+    return client
+        .createReceiver('$Default', partitionId, { 'startAfterTime': Date.now() })
+        .then((receiver) => {
+            console.log('Created partition receiver: ' + partitionId);
+            receiver.on('errorReceived', printError);
+            receiver.on('message', printMessage);
+        });
+}
+
+// Start server
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());
+app.get('/', function(req, res) {
+    
+        // Access the provided 'page' and 'limt' query parameters
+        const nRecords = parseInt(req.query.n);
+
+        if(nRecords) {
+            const renderValues = getDevValues(nRecords);
+            // Return the articles to the rendering engine
+            res.send(renderValues);
+        }
+    });
+
+app.listen(8080, () => console.log('Example app listening on port 8080'))
+
+// In case of TypeError in frames.js, see: https://github.com/noodlefrenzy/node-amqp10/issues/322
+if(connectToAzure) {
+    const client = EventHubClient.fromConnectionString(connectionString);
+    client
+        .open()
+        .then(client.getPartitionIds.bind(client))
+        .then((partitionIds) => {
+            return partitionIds.map(getClientReceiver);
+        })
+        .catch(printError);
+}
